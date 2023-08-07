e56001682f952cebbd2e216c2672c6c3c3db58aa
Alireza <aliibeygi7@gmail.com>
2023-08-07 16:19:43 +0330

    Add files of project

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+});
